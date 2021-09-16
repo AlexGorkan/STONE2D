3fa4a9941bae6058479e3f8951e11d7ebd0cf50a
Nekora Alexey <gorkan86@gmail.com>
2021-09-16 21:05:41 +0300

    Safe_position

---
+++ Stone Age Group2_2D/Assets/Scripts/UI/SafeArea.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+
+[RequireComponent(typeof(RectTransform))]
+public class SafeArea : MonoBehaviour
+{
+    
+    private void Awake()
+    {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        var safeArea = Screen.safeArea;
+
+        var anchorMin = safeArea.position;
+        var anchorMax = anchorMin + safeArea.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        rectTransform.anchorMin = anchorMin;
+        rectTransform.anchorMax = anchorMax;
+    }
+
+   
+}
