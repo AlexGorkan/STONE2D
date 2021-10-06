2b773472cf477d0d92fa9ac24dafbf5d3095a801
Alexey Nekora <gorkan86@gmail.com>
2021-10-06 22:15:26 +0300

    banana script test

---
+++ Stone Age Group2_2D/Assets/Scripts/Food/Banana.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Banana : Fructs
+{
+    public override void GetPoints()
+    {
+        Debug.Log("+10 POINTS");
+
+    }
+
+    
+}
